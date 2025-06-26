53357c3ca3d764413b30932aea981c3c4f63f9a8
Renzz-ustc <rzz18856919896@mail.ustc.edu.cn>
2025-06-27 00:31:53 +0800

    update FuseConfig,FuseConfigFetcher

--- doc/new/src/fuse/FuseLauncherConfig.cc
+++ doc/new/src/fuse/FuseLauncherConfig.cc
@@ -1,41 +1,41 @@
-// #include "FuseLauncherConfig.h"
-// #include <vector>
-// #include <string>
-// #include <stdexcept>
+#include "FuseLauncherConfig.h"
+#include <vector>
+#include <string>
+#include <stdexcept>
 
-// extern "C" {
-// struct FfiKeyValue {
-//     const char* key;
-//     const char* value;
-// };
-// int hf3fs_fuse_launcher_config_init(
-//     void* config,
-//     const char* file_path,
-//     bool dump,
-//     const FfiKeyValue* updates,
-//     size_t updates_len);
-// }
+extern "C" {
+struct FfiKeyValue {
+    const char* key;
+    const char* value;
+};
+int hf3fs_fuse_launcher_config_init(
+    void* config,
+    const char* file_path,
+    bool dump,
+    const FfiKeyValue* updates,
+    size_t updates_len);
+}
 
-// namespace hf3fs::fuse {
+namespace hf3fs::fuse {
 
-// void FuseLauncherConfig::init(const String &filePath, bool dump, const std::vector<config::KeyValue> &updates) {
-//     std::vector<FfiKeyValue> ffi_updates;
-//     std::vector<std::string> key_buf, value_buf;
-//     for (const auto& kv : updates) {
-//         key_buf.push_back(kv.first);
-//         value_buf.push_back(kv.second);
-//         ffi_updates.push_back(FfiKeyValue{key_buf.back().c_str(), value_buf.back().c_str()});
-//     }
-//     int ret = hf3fs_fuse_launcher_config_init(
-//         this,
-//         filePath.c_str(),
-//         dump,
-//         ffi_updates.data(),
-//         ffi_updates.size()
-//     );
-//     if (ret != 0) {
-//         throw std::runtime_error("Init launcher config failed (Rust FFI)");
-//     }
-// }
+void FuseLauncherConfig::init(const String &filePath, bool dump, const std::vector<config::KeyValue> &updates) {
+    std::vector<FfiKeyValue> ffi_updates;
+    std::vector<std::string> key_buf, value_buf;
+    for (const auto& kv : updates) {
+        key_buf.push_back(kv.first);
+        value_buf.push_back(kv.second);
+        ffi_updates.push_back(FfiKeyValue{key_buf.back().c_str(), value_buf.back().c_str()});
+    }
+    int ret = hf3fs_fuse_launcher_config_init(
+        this,
+        filePath.c_str(),
+        dump,
+        ffi_updates.data(),
+        ffi_updates.size()
+    );
+    if (ret != 0) {
+        throw std::runtime_error("Init launcher config failed (Rust FFI)");
+    }
+}
 
-// }  // namespace hf3fs::fuse
+}  // namespace hf3fs::fuse
--- doc/new/src/fuse/FuseLauncherConfig.h
+++ doc/new/src/fuse/FuseLauncherConfig.h
@@ -1,24 +1,24 @@
-// #pragma once
+#pragma once
 
-// #include "client/mgmtd/MgmtdClientForClient.h"
-// #include "common/app/NodeId.h"
-// #include "common/net/Client.h"
-// #include "common/utils/ConfigBase.h"
+#include "client/mgmtd/MgmtdClientForClient.h"
+#include "common/app/NodeId.h"
+#include "common/net/Client.h"
+#include "common/utils/ConfigBase.h"
 
-// namespace hf3fs::fuse {
-// struct FuseLauncherConfig : public ConfigBase<FuseLauncherConfig> {
-//   CONFIG_ITEM(cluster_id, "");
-//   CONFIG_OBJ(ib_devices, net::IBDevice::Config);
-//   CONFIG_OBJ(client, net::Client::Config);
-//   CONFIG_OBJ(mgmtd_client, client::MgmtdClientForClient::Config);
-//   CONFIG_ITEM(mountpoint, "");
-//   CONFIG_ITEM(allow_other, true);
-//   CONFIG_ITEM(token_file, "");
+namespace hf3fs::fuse {
+struct FuseLauncherConfig : public ConfigBase<FuseLauncherConfig> {
+  CONFIG_ITEM(cluster_id, "");
+  CONFIG_OBJ(ib_devices, net::IBDevice::Config);
+  CONFIG_OBJ(client, net::Client::Config);
+  CONFIG_OBJ(mgmtd_client, client::MgmtdClientForClient::Config);
+  CONFIG_ITEM(mountpoint, "");
+  CONFIG_ITEM(allow_other, true);
+  CONFIG_ITEM(token_file, "");
 
-//  public:
-//   using Base = ConfigBase<FuseLauncherConfig>;
-//   using Base::init;
+ public:
+  using Base = ConfigBase<FuseLauncherConfig>;
+  using Base::init;
 
-//   void init(const String &filePath, bool dump, const std::vector<config::KeyValue> &updates);
-// };
-// }  // namespace hf3fs::fuse
+  void init(const String &filePath, bool dump, const std::vector<config::KeyValue> &updates);
+};
+}  // namespace hf3fs::fuse
