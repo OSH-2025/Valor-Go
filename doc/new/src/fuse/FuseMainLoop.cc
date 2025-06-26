e3cdfcd32c0344ec1a8a961a147b5f37760fe48e
Renzz-ustc <rzz18856919896@mail.ustc.edu.cn>
2025-06-27 00:32:55 +0800

    update FuseMainLoop

--- doc/new/src/fuse/FuseMainLoop.cc
+++ doc/new/src/fuse/FuseMainLoop.cc
@@ -1,29 +1,29 @@
-// #include "FuseMainLoop.h"
-// #include <string>
+#include "FuseMainLoop.h"
+#include <string>
 
-// extern "C" {
-// int hf3fs_fuse_main_loop(
-//     const char* program_name,
-//     bool allow_other,
-//     const char* mountpoint,
-//     size_t maxbufsize,
-//     const char* cluster_id);
-// }
+extern "C" {
+int hf3fs_fuse_main_loop(
+    const char* program_name,
+    bool allow_other,
+    const char* mountpoint,
+    size_t maxbufsize,
+    const char* cluster_id);
+}
 
-// namespace hf3fs::fuse {
+namespace hf3fs::fuse {
 
-// int fuseMainLoop(const String &programName,
-//                  bool allowOther,
-//                  const String &mountpoint,
-//                  size_t maxbufsize,
-//                  const String &clusterId) {
-//     return hf3fs_fuse_main_loop(
-//         programName.c_str(),
-//         allowOther,
-//         mountpoint.c_str(),
-//         maxbufsize,
-//         clusterId.c_str()
-//     );
-// }
+int fuseMainLoop(const String &programName,
+                 bool allowOther,
+                 const String &mountpoint,
+                 size_t maxbufsize,
+                 const String &clusterId) {
+    return hf3fs_fuse_main_loop(
+        programName.c_str(),
+        allowOther,
+        mountpoint.c_str(),
+        maxbufsize,
+        clusterId.c_str()
+    );
+}
 
-// }  // namespace hf3fs::fuse
+}  // namespace hf3fs::fuse
